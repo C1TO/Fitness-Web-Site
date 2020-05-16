8f85501b25a95d9bf705d8172db4d68dbdfc33ba
Mıuhammed Çetin <61794760+C1TO@users.noreply.github.com>
2020-05-16 16:09:37 +0300

    Muhammed Çetin Login kısmı eklendi

---
+++ src/java/filter/LoginFilter.java
@@ -0,0 +1,58 @@
+package filter;
+
+
+import entity.Uye;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String url = req.getRequestURI();
+
+        Uye k = (Uye) req.getSession().getAttribute("valid_user");
+
+        if (k == null) {
+            if (url.contains("secret") || url.contains("logout")) {
+                res.sendRedirect(req.getContextPath() + "/login.xhtml");
+            } else {
+                chain.doFilter(request, response);
+            }
+        } else {
+            if (url.contains("register") || url.contains("login")) {
+                res.sendRedirect(req.getContextPath() + "/secret/secret.xhtml");
+            } else if (url.contains("logout")) {
+                req.getSession().invalidate();
+                res.sendRedirect(req.getContextPath() + "/index.xhtml");
+            } else {
+                chain.doFilter(request, response);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
